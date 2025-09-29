c7dfdbc17d4848cf6d5d2a4a383557d8383fa086
OscarLoZa05 <oscarlzafra@gmail.com>
2025-09-29 10:10:10 +0200

    Canvas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
@@ -6,6 +8,9 @@ public class GameManager : MonoBehaviour
     //Singelton /*Sirve para ser facil de acceder a cialqioer cosa, y evitar que haya duplicados de ese objeto*/ 
 
     public static GameManager instance { get; private set; } //Solo prodra cambiar le valor desde dentro del script para modificat el valor
+    [SerializeField] private GameObject _pauseCanvas;
+    [SerializeField] private InputActionAsset playerInputs;
+    private InputAction _pauseInput;
 
     private int _stars = 0;
 
@@ -21,11 +26,30 @@ public class GameManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
+
+        _pauseInput = InputSystem.actions["Pause"];
     }
 
     public void AddStar()
     {
         _stars++;
         Debug.Log("Estrella recogidas: " + _stars);
+    }
+
+    void Update()
+    {
+        if (_pauseInput.WasPressedThisFrame())
+        {
+            Pause();
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0;
+        _pauseCanvas.SetActive(true);
+        playerInputs.FindActionMap("Player").Disable();
+        
+        
     }
 }
